be8581232a7c16a7a6c44d9d4ac2584ae6c57cde
Dibyendu Jana <djana@redhat.com>
2019-05-27 18:00:54 +0530

    remove image stream creation

--- bin/setup_jenkins.sh
+++ bin/setup_jenkins.sh
@@ -16,7 +16,6 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 oc new-app jenkins-persistent --param MEMORY_LIMIT=2Gi --param VOLUME_CAPACITY=4Gi --param DISABLE_ADMINISTRATIVE_MONITORS=true -n ${GUID}-jenkins
 
 # Create custom agent container image with skopeo
-oc import-image docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11 -n ${GUID}-jenkins
 oc new-build -D $'FROM docker.io/openshift/jenkins-agent-maven-35-centos7:v3.11\nUSER root\nRUN yum -y install skopeo && yum clean all\nUSER 1001\n' --name='jenkins-agent-appdev' -n ${GUID}-jenkins
 
 # Create pipeline build config pointing to the ${REPO} with contextDir `openshift-tasks`
